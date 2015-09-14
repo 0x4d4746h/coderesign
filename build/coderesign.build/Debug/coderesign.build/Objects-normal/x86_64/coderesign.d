b869e731751d00fe1b6801589bd1789e26db1cfa
miaoguangfa <miaoguangfa@feinno.com>
2015-09-14 15:21:29 +0800

    fix bug

---
+++ build/coderesign.build/Debug/coderesign.build/Objects-normal/x86_64/coderesign.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/MacintoshHD/Projects/R&D/coderesign/coderesign/coderesign.m \
+  /Volumes/MacintoshHD/Projects/R&D/coderesign/coderesign/coderesign.h
---
+++ build/coderesign.build/Debug/coderesign.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Volumes/MacintoshHD/Projects/R&D/coderesign/coderesign/main.m \
+  /Volumes/MacintoshHD/Projects/R&D/coderesign/coderesign/coderesign.h
